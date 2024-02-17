8113b0a38ee589859efab40c1a1ac1c048af9d05
Dmitry Gutoryan <dmitry.gutoryan@sigli.com>
2024-02-17 17:31:59 +0100

    clean up the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'qt-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
-  title = 'quotes';
-}
+export class AppComponent {}
