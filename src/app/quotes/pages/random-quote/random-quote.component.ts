240a204ac1d06bd783fa78397dc97a4456d379e0
Dmitry Gutoryan <dmitry.gutoryan@sigli.com>
2024-02-17 20:33:56 +0100

    feat(quotes): add possibility to go to the next quote

--- src/app/quotes/pages/random-quote/random-quote.component.ts
+++ src/app/quotes/pages/random-quote/random-quote.component.ts
@@ -1,6 +1,8 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Subject, first } from 'rxjs';
 
 import { QuotesService } from '../../quotes.service';
+import { Quote } from '../../types';
 
 @Component({
   selector: 'qt-random-quote',
@@ -8,8 +10,23 @@ import { QuotesService } from '../../quotes.service';
   styleUrl: './random-quote.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RandomQuoteComponent {
-  public quote$ = this._quotesService.random();
+export class RandomQuoteComponent implements OnInit {
+  public quote$ = new Subject<Quote>();
 
   constructor(private readonly _quotesService: QuotesService) {}
+
+  ngOnInit(): void {
+    this._fetchQuote();
+  }
+
+  public nextQuote(): void {
+    this._fetchQuote();
+  }
+
+  private _fetchQuote(): void {
+    this._quotesService
+      .random()
+      .pipe(first())
+      .subscribe((quote) => this.quote$.next(quote));
+  }
 }
