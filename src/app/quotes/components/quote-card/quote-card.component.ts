27830c1820e692ead4325a81c3f3238381b65790
Dmitry Gutoryan <dmitry.gutoryan@sigli.com>
2024-02-17 18:33:01 +0100

    feat(quotes): init quote card

---
+++ src/app/quotes/components/quote-card/quote-card.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+
+import { Quote } from '../../types';
+
+@Component({
+  selector: 'qt-quote-card',
+  templateUrl: './quote-card.component.html',
+  styleUrl: './quote-card.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class QuoteCardComponent {
+  @Input({ required: true }) quote: Quote;
+}
--- src/app/quotes/quotes.module.ts
+++ src/app/quotes/quotes.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { QuotesRoutingModule } from './quotes-routing.module';
 import { RandomQuoteComponent } from './pages';
+import { QuoteCardComponent } from './components';
 
 @NgModule({
-  declarations: [RandomQuoteComponent],
+  declarations: [RandomQuoteComponent, QuoteCardComponent],
   imports: [CommonModule, QuotesRoutingModule],
 })
 export class QuotesModule {}
