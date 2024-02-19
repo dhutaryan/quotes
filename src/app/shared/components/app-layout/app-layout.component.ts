6ba02c73fa3e5aea1cb8f4fb411682acac2f0267
Dmitry Gutoryan <dmitry.gutoryan@sigli.com>
2024-02-19 14:16:59 +0100

    feat(layout): add nav menu

--- src/app/shared/components/app-layout/app-layout.component.ts
+++ src/app/shared/components/app-layout/app-layout.component.ts
@@ -1,10 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
+import { NavMenuComponent } from '../nav-menu/nav-menu.component';
+
 @Component({
   selector: 'qt-app-layout',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, NavMenuComponent],
   templateUrl: './app-layout.component.html',
   styleUrl: './app-layout.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
