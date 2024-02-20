85ebe8b8310e8aaa1b4ee6e9caa40bcb78723b6b
Dmitry Gutoryan <dmitry.gutoryan@sigli.com>
2024-02-20 01:03:16 +0100

    feat(layout): add header

--- src/app/shared/components/app-layout/app-layout.component.ts
+++ src/app/shared/components/app-layout/app-layout.component.ts
@@ -1,12 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
-import { NavMenuComponent } from '../nav-menu/nav-menu.component';
+import { HeaderComponent } from '../header';
 
 @Component({
   selector: 'qt-app-layout',
   standalone: true,
-  imports: [RouterModule, NavMenuComponent],
+  imports: [RouterModule, HeaderComponent],
   templateUrl: './app-layout.component.html',
   styleUrl: './app-layout.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
--- src/app/shared/components/index.ts
+++ src/app/shared/components/index.ts
@@ -2,3 +2,4 @@ export * from './app-layout';
 export * from './skeleton';
 export * from './nav-menu';
 export * from './pagination';
+export * from './header';
